5efe2da08a512d8e66a1c632b8e84610dd5cb197
Hola <winebrute161@gmail.com>
2025-05-10 23:28:09 +0300

    push

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import '../scss/style.scss';
-import '../js/swiper';
+import '../js/swiper.js';
 import '../js/menu.js';
 import '../js/modal.js';
-import '../js/variables';
+import '../js/variables.js';
 import '../js/btn-close.js';
 import '../js/swiper-btn.js';
